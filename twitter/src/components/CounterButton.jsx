2fce2002b7b53816a23a04e0769710dd3b04788f
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-02 00:06:31 +0330

    first component created

---
+++ twitter/src/components/CounterButton.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export function CounterButton() {
+
+    [counter, setClicks] = useState(0)
+
+    function addClicks() {
+        setClicks(counter + 1)
+    }
+
+    function reseClicks() {
+        resetCounter(0)
+    }
+
+
+    return <div>
+        <button onClick={addClicks}>
+            Touch me with that goddamn cursor
+        </button>
+        <button onClick={reseClicks}>
+            Click me to reset my feelings
+        </button>
+    </div>
+}
