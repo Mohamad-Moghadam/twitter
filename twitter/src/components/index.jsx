61898f3e770222fec43b3ada7617aa9ef7b80bb1
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-02 01:09:11 +0330

    🎉 index added and all the components are added to the app

---
+++ twitter/src/components/index.jsx
@@ -0,0 +1,13 @@
+import { CounterButton } from "./CounterButton";
+import { LikeMe } from "./LikeButton";
+import { PassTog } from "./PasswordToggle";
+import { SHText } from "./ShowHideText";
+
+export function Index() {
+    return <>
+    <CounterButton/>
+    <LikeMe/>
+    <SHText/>
+    <PassTog/>
+    </>
+}
