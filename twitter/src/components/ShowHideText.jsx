e235d794cf48380f80f70981fe90f1c3ce795c79
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-02 13:19:14 +0330

    🐛 show hide list fixed: the logic changed, styles imported

--- twitter/src/components/ShowHideText.jsx
+++ twitter/src/components/ShowHideText.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react"
+import Styles from "./SHtxt.module.css"
 
 export function SHText() {
 
-    const [classes, setClasses] = useState("show")
+    const [classes, setClasses] = useState(true)
 
     function changeClass() {
-        setClasses(prev => prev == "show" ? "hide" : "show")
+        setClasses(prev => !prev)
     }
 
 
 
     return <div>
-        <button>
+        <button onClick={changeClass}>
             Let me show sth
         </button>
-        <p  className={`${classes}`}>
+        <p  className={classes ? Styles.show : Styles.hide}>
             I'm supposed to be a plain txt but my feelings don't seem to matter. so click the button to ignore me!
         </p>
     </div>
